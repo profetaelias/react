9d9de57d1287f7273fe7713dbf4500c18b99375e
Elias Ribeiro <elias.paiva@dataprev.gov.br>
2019-11-04 16:58:29 -0300

    implementing the main application screen, components and some routes

--- my-money-app/frontend/src/main/app.jsx
+++ my-money-app/frontend/src/main/app.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import '../common/template/dependencies'
+
+import Header from '../common/template/header'
+import Sidebar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
+
+import Routes from './routes'
 
 export default props => (
     <div className='wrapper'>
-        <h1>App</h1>
+        <Header />
+        <Sidebar />
+        <div className='content-wrapper'>
+            <Routes />
+        </div>
+
+        <Footer />
     </div>
 )
