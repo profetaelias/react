9cf08401e8093996e94bd0968ad114febc035683
Elias Ribeiro <profetaelias@msn.com>
2019-11-04 13:19:41 -0300

    configuracao basica backend

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -3,9 +3,13 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(allowCors)
+app.use(queryParser())
 
 app.listen(port, function() {
     console.log(`BACKEND is running on port ${port}`)
