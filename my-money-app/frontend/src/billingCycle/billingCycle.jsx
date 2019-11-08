28722d00836f79fd793ebeab6372339adccf74cd
Elias Ribeiro <elias.paiva@dataprev.gov.br>
2019-11-08 15:48:25 -0300

    Starting class billingCycle

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
 
-export default props => (
-    <div>
-        <h1>Ciclo de Pagamento</h1>
-    </div>
-)
+export default class BillingCycle extends Component {
+    
+    
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Ciclo de Pagamentos' small='Cadastro'/>
+                <Content>
+                    Ciclos de Pagamentos
+                </Content>
+            </div>
+        )
+    }
+
+}
+    
